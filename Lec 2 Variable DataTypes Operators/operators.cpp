0c1364e9e0492876c3ca0a6317727f2f9681d07c
rvprakash9596 <raviprakash9596@gmail.com>
2024-09-15 20:37:56 +0530

    Operators in C++

--- Lec 2 Variable DataTypes Operators/operators.cpp
+++ Lec 2 Variable DataTypes Operators/operators.cpp
@@ -2,9 +2,35 @@
 using namespace std;
 int main(){
 
+//1.Arithmetic Operators(+,-,*,/,%)
 
+/*int a=15,b=10;
+cout<<"Sum = "<<(a+b)<<"\n";
+cout<<"Sub = "<<(a-b)<<"\n";
+cout<<"Mul = "<<(a*b)<<"\n";
+cout<<"Div = "<<(a/b)<<"\n";
+cout<<"Modulo = "<<(a%b)<<"\n";
+cout<<(5/(double)2)<<"\n";//2.5
 
+int ans = (5/(double)2);
+cout<<ans;//2 due to int data type
+*/
 
+//2.Relational Operator(<,<=,>,>=,==,!=)
+/*
+cout<<(3<5)<<"\n"; // true=1
+cout<<(3>5)<<"\n"; // false=0
+cout<<(3<=5)<<"\n";//1
+cout<<(3>=5)<<"\n";//0
+cout<<(3==5)<<"\n";//0
+cout<<(3!=5)<<"\n";//1
+*/
 
-    return 0;
+//3. Logical Operator (||,&&,!)
+cout<<!(3<1)<<"\n";//1
+cout<<!(3<1)<<"\n";//1
+cout<<((3>1)||(3<5))<<"\n";//1
+cout<<((3>1)&&(5<3))<<"\n";//0
+
+return 0;
 }
