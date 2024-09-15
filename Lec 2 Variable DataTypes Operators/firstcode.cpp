0a97e884194a6ab75f0660ba28448534b7646ef0
rvprakash9596 <raviprakash9596@gmail.com>
2024-09-15 11:33:08 +0530

    This is First Code

---
+++ Lec 2 Variable DataTypes Operators/firstcode.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;// std is a namespace
+
+int main(){
+cout<<"This is first C++ Program \n";
+cout<<"This is first C++ Program "<<endl;
+cout<<"This is first C++ Program"<<"\n";
+return 0;
+}
