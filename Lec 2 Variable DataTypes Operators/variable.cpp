a7c08623a71b2f2e75d4b6e17c2c092b3bd431c6
rvprakash9596 <raviprakash9596@gmail.com>
2024-09-15 20:09:06 +0530

    Variables in c++

---
+++ Lec 2 Variable DataTypes Operators/variable.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //int data types
+    int age =21;
+    cout<<"My age is :"<< age<<"\n";
+    cout<<"Size of Age is : "<<sizeof(age)<<" bytes"<<"\n" ;
+
+    //char data type
+    char grade='A';
+    cout<<"Grade is : "<<grade<<"\n";
+    
+    //float data type
+    float pi=3.14f;
+    cout<<pi<<"\n";
+
+    //bool data type
+    bool isSafe=true;
+    cout<<isSafe<<"\n";//1
+    // true=1
+    //false=0
+
+
+    //double data type
+    double salary=45500.68;
+    cout<<"My salary is : "<<salary<<"\n";
+    return 0;
+}
