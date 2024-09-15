11e87822377b85f8007916ee537ea296a804c2f9
rvprakash9596 <raviprakash9596@gmail.com>
2024-09-15 11:50:00 +0530

    Print Our Name Program

---
+++ Lec 2 Variable DataTypes Operators/PrintName.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+//This is the starting point of execution
+int main(){
+    cout<<"Hello";
+    cout<<"Ravi\n";
+    cout<<"Prakash\n";
+    cout<<"Singh";
+    return 0;
+}
