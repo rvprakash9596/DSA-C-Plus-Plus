06394ab99c53645f12114ac0644637bd2b6121de
rvprakash9596 <raviprakash9596@gmail.com>
2024-09-18 18:23:08 +0530

    Variables

--- Lec 3/Variables.cpp
+++ Lec 3/Variables.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a;
-    a=10;
+
+    /*
+    int a;//Variable Declaration
+    a=10;//Variable Initialization
     cout<<"The value of a is :"<<a;
+    */
+
+   int a=10;
+   cout<<"The value of a is :"<<a;
+
 }
