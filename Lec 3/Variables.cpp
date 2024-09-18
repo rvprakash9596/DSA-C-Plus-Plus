00cf3d82a19d4a116a7d8d1cd5fc7faf6129cbfb
rvprakash9596 <raviprakash9596@gmail.com>
2024-09-18 18:28:11 +0530

    Variables

--- Lec 3/Variables.cpp
+++ Lec 3/Variables.cpp
@@ -8,7 +8,31 @@ int main(){
     cout<<"The value of a is :"<<a;
     */
 
-   int a=10;
-   cout<<"The value of a is :"<<a;
+/*
+   int a=10,b=20,c=30,sum=(a+b+c);
+   cout<<"The value of a is :"<<sum<<endl;
+*/
+
+/*
+   char c='R';
+   cout<<c;
+*/
+
+/*
+    float f = 2.3f;
+    cout<<f;
+*/
+
+/*
+double d=5.654784;
+cout<<d;
+*/
+
+bool b1=true;
+bool b2=false;
+cout<<b1;
+cout<<b2;
+
+
 
 }
