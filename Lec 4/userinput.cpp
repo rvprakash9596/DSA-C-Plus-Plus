6b97172fc010f8464b9cc930c3978d0ad7b6eb16
rvprakash9596 <raviprakash9596@gmail.com>
2024-09-20 22:27:09 +0530

    User Input in C++

---
+++ Lec 4/userinput.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,sum;
+    cout<<"Enter 1st Number :";
+    cin>>a;
+    cout<<"Enter 2nd Number :";
+    cin>>b;
+    sum=a+b;
+    cout<<"Sum ="<<sum;
+}
